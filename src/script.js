e790de33c8a2190dfe5795290c8196b4eb84095d
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-01 12:54:07 +0000

    Built the logic

--- src/script.js
+++ src/script.js
@@ -6,6 +6,11 @@ const score = {
     player2: 0,
 };
 
+const symbol = {
+    player1: "",
+    player2: "",
+};
+
 startBtn.addEventListener("click", () => {
     enableBtn("restart");
 });
@@ -16,12 +21,15 @@ reStartBtn.addEventListener("click", () => {
 
 allCtrBtns.forEach((item) => {
     item.addEventListener("click", (e) => {
-        console.log(e.target.textContent.trim().toLowerCase());
+        symbol["player1"] = e.target.textContent.trim().toLowerCase();
+        symbol["player2"] = choseRPS();
+        if (symbol["player1"] !== "" && symbol["player2"] !== "") checkWinner();
     });
 });
 
 function enableBtn(btnName) {
     if (btnName === "restart") {
+        alert("Game Started");
         reStartBtn.classList.remove("hidden");
         startBtn.classList.add("hidden");
         allCtrBtns.forEach((item) => {
@@ -33,5 +41,42 @@ function enableBtn(btnName) {
         allCtrBtns.forEach((item) => {
             item.disabled = true;
         });
+    }
+}
+
+function choseRPS() {
+    const options = ["rock", "paper", "scissors"];
+    const i = Math.floor(Math.random() * 3);
+    return options[i];
+}
+
+function checkWinner() {
+    if (symbol["player1"] === symbol["player2"]) {
+        console.log("Tie");
+        return;
+    } else if (
+        symbol["player1"] === "rock" &&
+        symbol["player2"] === "scissors"
+    ) {
+        score["player1"]++;
+        console.log("Player 1 Wons: rock beats scissors");
+        return;
+    } else if (symbol["player1"] === "paper" && symbol["player2"] === "rock") {
+        score["player1"]++;
+        console.log("Player 1 Wons: paper beats rock");
+        return;
+    } else if (
+        symbol["player1"] === "scissors" &&
+        symbol["player2"] === "paper"
+    ) {
+        score["player1"]++;
+        console.log("Player 1 Wons: scissors beats paper");
+        return;
+    } else {
+        score["player2"]++;
+        console.log(
+            `Player 2 won: ${symbol["player2"]} beats ${symbol["player1"]}`
+        );
+        return;
     }
 }
